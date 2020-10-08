6947f130bba24d01c6acea76866e3d2afae71dbc
angelo milien <angelomilienjoseph@gmail.com>
2020-10-07 20:00:24 -0400

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
 def oxford_comma(array)
+   if array.length == 1
+    array.join
 
+   elsif array.length == 2
+    array.join(" and ")
+
+   elsif array.length == 3
+    array[2] = "and starfruit"
+    array.join(", ")
+
+   elsif array.length == 5
+    array[4] = "and dragon fruits"
+    array.join(", ")
+
+   elsif array.length == 7
+    array[6] = "and pomelos"
+    array.join(", ")
+   end
+
+   
 end
+
